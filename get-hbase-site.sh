80424c1d57328081fcb37d4a6d17f9836206f61c
tedyu <yuzhihong@gmail.com>
2014-07-09 12:53:23 -0700

    SLIDER-220 Provide script for retrieving hbase-site.xml given cluster name

--- get-hbase-site.sh
+++
@@ -1,8 +0,0 @@
-tuple=`slider status $1 | grep "info.am.web.url"`
-FS=":"
-url=`echo $tuple | awk '{split($0,array,": ")} END{print array[2]}'`
-url="${url%,}"
-url="${url%\"}"
-url="${url#\"}"
-url="${url}ws/v1/slider/publisher/slider/hbase-site.xml"
-curl -k -o hbase-site.xml $url
