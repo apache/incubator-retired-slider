14ab631e4f07d258036eec9db916e132abb63ef4
Steve Loughran <stevel@apache.org>
2015-06-05 09:43:16 +0100

    SLIDER-898 coalesce use of assumeWindows/assumeNotWindows test assumptions

--- slider-funtest/src/test/groovy/org/apache/slider/funtest/commands/CommandEnvironmentIT.groovy
+++ slider-funtest/src/test/groovy/org/apache/slider/funtest/commands/CommandEnvironmentIT.groovy
@@ -90,7 +90,7 @@ public class CommandEnvironmentIT extends CommandTestBase {
     if (python) {
       SliderShell.scriptFile = SLIDER_SCRIPT_PYTHON;
     } else {
-      assume(!SliderShell.windows, "skip bash test on windows")
+      assumeNotWindows()
       SliderShell.scriptFile = SLIDER_SCRIPT;
     }
     SliderShell shell = new SliderShell([
